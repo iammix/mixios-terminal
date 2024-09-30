92a9ae902ea8b16297454ded6a87dd4d16259037
Konstantinos Mixios <k.mixios@gmail.com>
2024-09-30 13:13:09 +0300

    UPDATE

--- src/components/commands/Projects.tsx
+++ src/components/commands/Projects.tsx
@@ -39,8 +39,7 @@ const Projects: React.FC = () => {
   ) : (
     <div data-testid="projects">
       <ProjectsIntro>
-        “Talk is cheap. Show me the code”? I got you. <br />
-        Here are some of my projects you shouldn't misss
+        TODO: A list of selected projects will be displayed here
       </ProjectsIntro>
       {projects.map(({ id, title, desc }) => (
         <ProjectContainer key={id}>
@@ -53,31 +52,13 @@ const Projects: React.FC = () => {
   );
 };
 
-const projects = [
-  {
-    id: 1,
-    title: "Sat Naing's Blog",
-    desc: "My personal blog where I can write down my thoughts and experiences.",
-    url: "https://satnaing.dev/blog/",
-  },
-  {
-    id: 2,
-    title: "Haru Fashion",
-    desc: "An ecommerce web application where users can browse various products and make purchases.",
-    url: "https://haru-fashion.vercel.app/",
-  },
-  {
-    id: 3,
-    title: "Haru API",
-    desc: "A RESTful API developed for the Haru fashion ecommerce project.",
-    url: "https://satnaing.github.io/haru-api/",
-  },
-  {
-    id: 4,
-    title: "AstroPaper Blog Theme",
-    desc: "A minimal, accessible and SEO-friendly Astro blog theme.",
-    url: "https://astro-paper.pages.dev/",
-  },
-];
+const projects = [];
+//   {
+//     id: 1,
+//     title: "Sat Naing's Blog",
+//     desc: "My personal blog where I can write down my thoughts and experiences.",
+//     url: "https://satnaing.dev/blog/",
+//   },
+// ];
 
 export default Projects;
